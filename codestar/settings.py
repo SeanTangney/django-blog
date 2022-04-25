2149199e4ea86c122d595ff65910f1f44185d208
SeanTangney <sean_tangney@hotmail.com>
2022-04-25 18:30:35 +0000

    Fixed typo in settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -134,7 +134,7 @@ STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_url = '/static/'
+MEDIA_URL = '/static/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
